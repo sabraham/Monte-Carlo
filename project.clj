51111888c97335072ccb86c5c6b42523bcc88e54
Sunil Abraham <s.abraham@gmail.com>
2013-07-02 22:24:34 -0400

    Add local maven repo and hand evaluator jar

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :repositories {"local" "file:repo"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [local/spears "0.0.1"]]
+)
