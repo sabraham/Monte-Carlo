d3dbd955c7478fbd2332e35a9e900f75e8946fa6
Sunil Abraham <s.abraham@gmail.com>
2013-07-03 18:03:57 -0400

    Working update-bets, and lots of defrecords for poker structures

--- src/montecarlo/core.clj
+++ src/montecarlo/core.clj
@@ -1,6 +1,120 @@
-(ns montecarlo.core)
+(ns montecarlo.core
+  (require [clojure.core.async :as async
+            :refer [<! >! <!! >!! timeout chan alt! alts!! go]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Card
+    [suit rank])
+
+(defrecord Bet
+    [bet players])
+
+(defrecord Pot
+    [pot players])
+
+(defrecord Player
+    [id stack])
+
+(defrecord Board
+    [deck
+     community-cards
+     bets    ;; list of Bet
+     pots    ;; list of Pot
+     remaining-players ;; players who have not yet played
+     players ;; players still in hand
+     dealer  ;; Player that is dealer
+     blinds])
+
+(def COMPLETE-DECK
+  (for [suit [:hearts :diamonds :spades :clubs]
+        rank (range 2 15)]
+    (Card. suit rank)))
+
+(defn init-board
+  [players dealer blinds]
+  (let [deck COMPLETE-DECK
+        community-cards (list)
+        folded (set)
+        pots (list)
+        bets []
+        remaining-players players]
+    (Board. deck community-cards bets pots
+            remaining-players players dealer blinds)))
+
+
+(defn is-fold?
+  [action]
+  (neg? action))
+
+(defn is-call?
+  [action]
+  (= action 0))
+
+(defn is-raise?
+  [action]
+  (pos? action))
+
+(defn fold
+  [player board]
+  (let [remaining-players (remove #{player} (:remaining-players board))]
+    (assoc board :remaining-players remaining-players)))
+
+(defn action->raise
+  [r]
+  r)
+
+(defn split-bet
+  [bet x player]
+  [(Bet. x (conj (:players bet) player))
+   (update-in bet [:bet] #(- % x))])
+
+(defn call-bet
+  [bets player]
+  (update-in bets [0 :players] #(conj % player)))
+
+(defn update-bets
+  [bets new-bet]
+  (let [[player] (get new-bet :players)
+        bet (get new-bet :bet)]
+    (loop [bets bets
+           bet bet
+           ret []]
+      (if-let [standing-bet (first bets)]
+        (cond
+         (< bet (:bet standing-bet)) (concat ret (split-bet standing-bet bet player))
+         (= bet (:bet standing-bet)) (concat ret (call-bet bets player))
+         (> bet (:bet standing-bet)) (recur (rest bets)
+                                            (- bet (:bet standing-bet))
+                                            (conj ret (first (call-bet bets player)))))
+        (concat ret [(Bet. bet #{player})])))))
+
+(defn raise
+  [board player r]
+  (let [bet (+ r (get-in board [:bet]))]))
+
+(defn call
+  [board player]
+  (raise board player 0))
+
+(comment
+  (defn play-stage
+    [board stage-end stage-transition action-ch broadcast-ch]
+    (loop [board board
+           remaining-cycle (cycle (:remaining-players board))]
+      (let [player (first remaining-cycle)]
+        (if (stage-end board)
+          (stage-transition board)
+          (go
+           (let [action (<!! action-ch)]
+             (cond
+              (is-fold? action) (recur (fold board)
+                                       (rest remaining-cycle))
+              (is-call? action) (recur (call board player)
+                                       (rest remaining-cycle))
+              (is-raise? action (recur (raise board (action->raise action))
+                                       (rest remaining-cycle)))
+              :else (throw "Action is not fold nor call nor raise!"))))))))
+
+  (defn preflop-board->flop-board
+    "First player and current player reset"
+    [board]
+    (let [])))
--- test/montecarlo/core_test.clj
+++ test/montecarlo/core_test.clj
@@ -2,6 +2,23 @@
   (:use clojure.test
         montecarlo.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-update-bets
+  (let [p1 (->Player 1 100)
+        p2 (->Player 2 200)
+        p3 (->Player 3 150)
+        b1 (->Bet 10 #{p1})
+        b1a (->Bet 10 #{p1 p2})
+        b2a (->Bet 10 #{p2})
+        b1b (->Bet 10 #{p1 p2 p3})
+        b2b (->Bet 5 #{p2 p3})
+        b3b (->Bet 5 #{p2})]
+    (testing "initial bet"
+      (is (= [b1]
+             (update-bets [] (->Bet 10 [p1])))))
+    (testing "additional bet"
+      (is (= [b1a b2a]
+             (update-bets [b1] (->Bet 20 [p2])))))
+    (testing "split earlier bet"
+      (is (= [b1b b2b b3b]
+             (update-bets [b1a b2a] (->Bet 15 [p3])))))))
+
