48a5067f2f7b6ba129c4ba24adfb231f334d6420
Sunil Abraham <s.abraham@gmail.com>
2013-07-29 14:53:29 -0400

    License + Aleph

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject montecarlo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"}
   :repositories {"local" "file:repo"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [local/spears "0.0.1"]
+                 ;;[local/spears "0.0.1"]
                  [core.async "0.1.0-SNAPSHOT"]
-                 [org.clojure/math.combinatorics "0.0.4"]]
+                 [org.clojure/math.combinatorics "0.0.4"]
+                 [aleph "0.3.0-rc2"]]
   :jvm-opts ["-Xmx1g" "-server"]
 )
