fcffb0f4c5a8a08b74abae422017075e61e944db
Sunil Abraham <s.abraham@gmail.com>
2013-08-02 18:09:46 -0400

    Increased validation of user input
    create new room
    - room name empty?
    - room name exists
    - number of players is positive int
    - blinds are all positive ints
    
    join room
    - room name exists

--- src/montecarlo/core.clj
+++ src/montecarlo/core.clj
@@ -476,23 +476,75 @@
 
 (def DATABASE (atom {}))
 
+(defn error-msg
+  [p status-code msg]
+  (go (>! (:out-ch p) (generate-string {:status status-code :msg msg}))))
+
+(defn ok-msg
+  [p]
+  (go (>! (:out-ch p) (generate-string {:status 0 :msg "OK"}))))
+
+(defn pos-int?
+  [x]
+  (and (integer? x) (pos? x)))
+
+(defn valid-blinds?
+  [p blinds]
+  (let [blind-vals (vals blinds)]
+    (if (every? pos-int? blind-vals)
+      true
+      (do
+        (error-msg p -12 "Blinds must be positive integers")
+        false))))
+
+(defn valid-num-players?
+  [p n]
+  (println (str "in this " n))
+  (if (pos-int? n)
+    true
+    (do
+      (error-msg p -5 "n (number of players) must be a postive integer.")
+      false)))
+
+(defn valid-room?
+  [p name]
+  (if (get @DATABASE name)
+    (do
+      (error-msg p -5 "room already exists.")
+      false)
+    (if (nil? name)
+      (do
+        (error-msg p -5 "empty room name.")
+        false)
+      true)))
+
 ;; TODO race condition
 (defn create-new-room
-  [name num-players]
-  (let [q (chan (buffer num-players))]
-    (swap! DATABASE assoc name {:players (list)
-                                :n num-players
-                                :q q})
-    (go (while true
-          (alt! q ([p] (do
-                         (swap! DATABASE #(update-in % [name :players] conj p))
-                         (when (= (count (get-in @DATABASE [name :players])) num-players)
-                           (game (get-in @DATABASE [name :players])
-                                 {:small 5 :big 10})))))))))
+  [p {:keys [name n blinds] :or {blinds {:small 5 :big 10}}}]
+  (println (str name " " n " " blinds))
+  (when (and (valid-room? p name)
+             (valid-num-players? p n)
+             (valid-blinds? p blinds))
+    (let [q (chan (buffer n))]
+      (swap! DATABASE assoc name {:players (list)
+                                  :n n
+                                  :q q})
+      (go (while true
+            (alt! q ([p] (do
+                           (swap! DATABASE #(update-in % [name :players] conj p))
+                           (println (str "count: " (count (get-in @DATABASE [name :players])) " " n))
+                           (when (= (count (get-in @DATABASE [name :players])) n)
+                             (game (get-in @DATABASE [name :players])
+                                   blinds)))))))
+      (ok-msg p))))
 
 (defn join-room
-  [p room]
-  (go (>! (get-in @DATABASE [room :q]) p)))
+  [p {:keys [name]}]
+  (if-let [q (get-in @DATABASE [name :q])]
+    (do
+      (go (>! q p))
+      (ok-msg p))
+    (error-msg -2 (str "Room \"" name "\" does not exist yet."))))
 
 (defn handler
   [ch client-info]
@@ -500,13 +552,15 @@
                     (chan (sliding-buffer 1))
                     (chan) (chan) (chan) (chan))]
     (println "new connection")
-    (receive-all ch #(let [req (parse-string %)]
-                       (case (get req "type")
-                         "new_room" (create-new-room (get req "name") (get req "n"))
-                         "join_room" (join-room p (get req "name"));;(players-waiting-fn p) ;;     (go (>! PLAYERS-WAITING p))
-                         "play" (go (>! (:listen-ch p) (get req "amt")))
-                         (go (>! (:out-ch p) (generate-string {:error -1}))))
-                       (println %)))
+    (receive-all ch #(try
+                       (let [req (clojure.walk/keywordize-keys (parse-string %))]
+                         (case (:type req)
+                           "new_room" (create-new-room p req)
+                           "join_room" (join-room p req)
+                           "play" (go (>! (:listen-ch p) (:amt req)))
+                           (error-msg p -1 "bad \"type\" argument")))
+                       (catch com.fasterxml.jackson.core.JsonParseException e
+                         (error-msg p -17 "You sent me bad json!"))))
     (go
      (while true
        (alt!
